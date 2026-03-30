2569512feeeb92b29dca056241dd46fb82df05d8
Franciscojmara <franciscomartnez54@gmail.com>
2026-03-30 10:32:39 -0600

    Move hyper-paramters from main file to preamble.

--- 00_Preamble.R
+++ 00_Preamble.R
@@ -14,6 +14,7 @@ missing <- setdiff(packages, rownames(installed.packages()))
 install.packages(missing, dependencies = TRUE)
 lapply(packages, library, character.only = TRUE)
 
+
 # Directories
 dataPath <- file.path(getwd(), "Data")
 figsPath <- file.path(getwd(), "Figures")
@@ -24,6 +25,27 @@ dname <- "Data/Preprocessed"
 if(!dir.exists(dname)) dir.create(dname, recursive = TRUE)
 if(!dir.exists(figsPath)) dir.create(figsPath, recursive = TRUE)
 if(!dir.exists(resuPath)) dir.create(resuPath, recursive = TRUE)
+
+
+# Hyper-parameters: Data construction
+start_date <- "2003-12-01" # INPC start date (so inflation starts on 2001m1)
+end_date   <- "2024-12-01" # INPC end date
+climate_db <- "WorldBank"  # Climate data source: "WorldBank", or "CONAGUA"
+data_freq  <- "Quarterly"  # Frequency of data: "Monthly", or "Quarterly"
+infvar     <- "q"          # monthly (m), quarterly (q), or yearly (y) inflation
+no_regions <- 07           # Number of Mexican regions [4, 7, or 32]
+sead_adjst <- TRUE         # TRUE to seasonally adjusted INPC/ITAEE data
+center_dta <- FALSE        # Center data distribution?
+weight_dta <- TRUE         # Use population-weighted data?
+ma.means   <- c(20,30,40)  # Years to consider for the historical norm (mean)
+faccia.def <- FALSE        # Use the climate anomalies definition of Faccia?
+tempdev    <- 0.02         # °C deviation significant weighted temp.deviations
+const.data <- TRUE         # Reconstruct data-set? (Required if first time run)
+
+# Hyper-parameters: Econometrics
+IRF.hmax   <- 2            # Max horizon for impulse-responses in years 
+alpha_LPs  <- 0.05         # Confidence level to estimate LPs confidence int.
+
 
 # Change ma.mean in case of CONAGUA data
 if(climate_db == "CONAGUA") ma.means <- 15
--- MASTER.R
+++ MASTER.R
@@ -5,28 +5,7 @@
 ## ========================================================================== ##
 
 ## ========================================================================== ##
-## Hyper-parameters ---------------------------------------------------------
-## ========================================================================== ##
-
-start_date <- "2003-12-01" # INPC start date (so inflation starts on 2001m1)
-end_date   <- "2024-12-01" # INPC end date
-climate_db <- "WorldBank"  # Climate data source: "WorldBank", or "CONAGUA"
-data_freq  <- "Quarterly"  # Frequency of data: "Monthly", or "Quarterly"
-infvar     <- "q"          # monthly (m), quarterly (q), or yearly (y) inflation
-no_regions <- 07           # Number of Mexican regions [4, 7, or 32]
-sead_adjst <- TRUE         # TRUE to seasonally adjusted INPC/ITAEE data
-center_dta <- FALSE        # Center data distribution?
-weight_dta <- TRUE         # Use population-weighted data?
-ma.means   <- c(20,30,40)  # Years to consider for the historical norm (mean)
-IRF.hmax   <- 2            # Max horizon for impulse-responses in years 
-alpha_LPs  <- 0.05         # Confidence level to estimate LPs confidence int.
-faccia.def <- FALSE        # Use the climate anomalies definition of Faccia?
-tempdev    <- 0.02         # °C deviation significant weighted temp.deviations
-const.data <- TRUE         # Reconstruct data-set? (Required if first time run)
-
-
-## ========================================================================== ##
-## Main analysis ------------------------------------------------------------
+## Set up and create data ---------------------------------------------------
 ## ========================================================================== ##
 
 ## *** Packages, options, and paths
@@ -44,7 +23,9 @@ if (const.data) {
   source("02_Merge_Macro-Climate-data_Regions.R")
 }
 
-## *** Econometric analysis
+## ========================================================================== ##
+## Econometric analysis -----------------------------------------------------
+## ========================================================================== ##
 
 # Descriptive plots and maps
 source("10_DescriptivePlots_Economic-Climate_Regions.R")
